74313e85e8d7f384525925bc02b2c99e4c36d013
Олег Дунаев <o.dunaev@gw.spb.ru>
2018-06-19 16:12:03 +0300

    ask445

--- js/main.js
+++ js/main.js
@@ -1,29 +1,35 @@
 var canvas = this.__canvas = new fabric.StaticCanvas('c');
-var canvasBG_color_url = 'assets/img/black';
+var canvasBG_color_url = 'assets/img/white';
 var canvasBG_side_url = '/_front.png';
 var canvasBG_url = canvasBG_color_url + canvasBG_side_url;
 
 
-function changeCanvasBgUrl(){
-	return canvasBG_url = canvasBG_color_url + canvasBG_side_url;
+
+function colorChange(color){
+	canvasBG_color_url = 'assets/img/' + color;
+	changeCanvasBgUrl();
+	renderTshirt();
 };
 
 function flipToFront() {
 	canvasBG_side_url = '/_front.png';
 	changeCanvasBgUrl();
-	sdsd();
+	renderTshirt();
 };
 function flipToBack() {
 	canvasBG_side_url = '/_back.png';
 	changeCanvasBgUrl();
-	sdsd();
-	
+	renderTshirt();
 };
 
-function sdsd() {
+function changeCanvasBgUrl(){
+	return canvasBG_url = canvasBG_color_url + canvasBG_side_url;
+};
 
+function renderTshirt() {
 	fabric.Image.fromURL(canvasBG_url, function(img) {
 		canvas.add(img.set({ left: 0, top: 0, angle: 0, selectable:false }).scale(1));
 	});
-
 };
+
+renderTshirt();
