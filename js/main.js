07f3e5da8e94a4649e7b6c886660279f2795e377
Олег Дунаев <o.dunaev@gw.spb.ru>
2018-06-19 14:47:35 +0300

    flip

--- js/main.js
+++ js/main.js
@@ -1,9 +1,29 @@
-(function() {
 var canvas = this.__canvas = new fabric.StaticCanvas('c');
+var canvasBG_color_url = 'assets/img/black';
+var canvasBG_side_url = '/_front.png';
+var canvasBG_url = canvasBG_color_url + canvasBG_side_url;
 
-fabric.Image.fromURL('assets/img/black_front.png', function(img) {
-	canvas.add(img.set({ left: 0, top: 0, angle: 0 }).scale(1));
-});
 
+function changeCanvasBgUrl(){
+	return canvasBG_url = canvasBG_color_url + canvasBG_side_url;
+};
 
-})();
+function flipToFront() {
+	canvasBG_side_url = '/_front.png';
+	changeCanvasBgUrl();
+	sdsd();
+};
+function flipToBack() {
+	canvasBG_side_url = '/_back.png';
+	changeCanvasBgUrl();
+	sdsd();
+	
+};
+
+function sdsd() {
+
+	fabric.Image.fromURL(canvasBG_url, function(img) {
+		canvas.add(img.set({ left: 0, top: 0, angle: 0, selectable:false }).scale(1));
+	});
+
+};
