14434e84b3b8110ac017cbe27746e863d2705109
Олег Дунаев <o.dunaev@gw.spb.ru>
2018-06-20 15:11:05 +0300

    rework

--- js/main.js
+++ js/main.js
@@ -1,35 +1,59 @@
-var canvas = this.__canvas = new fabric.StaticCanvas('c');
 var canvasBG_color_url = 'assets/img/white';
-var canvasBG_side_url = '/_front.png';
-var canvasBG_url = canvasBG_color_url + canvasBG_side_url;
+// var canvasBG_side_url = '/_front.png';
+var canvasBG_front_url = canvasBG_color_url + '/_front.png';
+var canvasBG_back_url = canvasBG_color_url + '/_back.png';
+
+$( ".flip-switcher" ).click(function() {
+	$( ".toggle" ).toggle();
+});
 
 
+var canvasFront =  new fabric.StaticCanvas('front', {
+	width:480,
+	height:510,
+});
+
+var canvasBack =  new fabric.StaticCanvas('back', {
+	width:480,
+	height:510,
+});
+
+
+
+
+
+
+refreshShirtColor();
 
 function colorChange(color){
 	canvasBG_color_url = 'assets/img/' + color;
 	changeCanvasBgUrl();
-	renderTshirt();
-};
-
-function flipToFront() {
-	canvasBG_side_url = '/_front.png';
-	changeCanvasBgUrl();
-	renderTshirt();
-};
-function flipToBack() {
-	canvasBG_side_url = '/_back.png';
-	changeCanvasBgUrl();
-	renderTshirt();
+	refreshShirtColor();
+	return canvasBG_color_url,canvasBG_front_url,canvasBG_back_url;
 };
 
 function changeCanvasBgUrl(){
-	return canvasBG_url = canvasBG_color_url + canvasBG_side_url;
+	canvasBG_front_url = canvasBG_color_url + '/_front.png';
+	canvasBG_back_url = canvasBG_color_url + '/_back.png';
+	return canvasBG_front_url,canvasBG_back_url;
 };
 
-function renderTshirt() {
-	fabric.Image.fromURL(canvasBG_url, function(img) {
-		canvas.add(img.set({ left: 0, top: 0, angle: 0, selectable:false }).scale(1));
-	});
+
+
+
+function refreshShirtColor(){
+	canvasFront.setBackgroundImage(canvasBG_front_url, canvasFront.renderAll.bind(canvasFront));
+	canvasBack.setBackgroundImage(canvasBG_back_url, canvasBack.renderAll.bind(canvasBack));
 };
 
-renderTshirt();
+
+// fabric.Image.fromURL(canvasBG_front_url , function(img){
+// 	canvasFront.add(img);
+// 	img.center();
+// });
+
+// fabric.Image.fromURL(canvasBG_back_url , function(img){
+// 	canvasBack.add(img);
+// 	img.center();
+// });
+
