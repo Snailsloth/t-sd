b6817ab57f9748052811f125283a812cf8ea69d8
Олег Дунаев <o.dunaev@gw.spb.ru>
2018-06-20 16:26:27 +0300

    adding arts

--- js/main.js
+++ js/main.js
@@ -3,16 +3,17 @@ var canvasBG_color_url = 'assets/img/white';
 var canvasBG_front_url = canvasBG_color_url + '/_front.png';
 var canvasBG_back_url = canvasBG_color_url + '/_back.png';
 
+//toggle front-back shirt image
 $( ".flip-switcher" ).click(function() {
 	$( ".toggle" ).toggle();
 });
 
-
-var canvasFront =  new fabric.StaticCanvas('front', {
+//tshirt front
+var canvasFront =  new fabric.Canvas('front', {
 	width:480,
 	height:510,
 });
-
+//tshirt front
 var canvasBack =  new fabric.StaticCanvas('back', {
 	width:480,
 	height:510,
@@ -22,38 +23,56 @@ var canvasBack =  new fabric.StaticCanvas('back', {
 
 
 
-
+//rerender with new links with chosen colors
 refreshShirtColor();
-
+//change color links after option input
 function colorChange(color){
 	canvasBG_color_url = 'assets/img/' + color;
 	changeCanvasBgUrl();
 	refreshShirtColor();
 	return canvasBG_color_url,canvasBG_front_url,canvasBG_back_url;
 };
-
 function changeCanvasBgUrl(){
 	canvasBG_front_url = canvasBG_color_url + '/_front.png';
 	canvasBG_back_url = canvasBG_color_url + '/_back.png';
 	return canvasBG_front_url,canvasBG_back_url;
 };
-
-
-
-
 function refreshShirtColor(){
 	canvasFront.setBackgroundImage(canvasBG_front_url, canvasFront.renderAll.bind(canvasFront));
 	canvasBack.setBackgroundImage(canvasBG_back_url, canvasBack.renderAll.bind(canvasBack));
 };
 
 
-// fabric.Image.fromURL(canvasBG_front_url , function(img){
-// 	canvasFront.add(img);
-// 	img.center();
-// });
 
-// fabric.Image.fromURL(canvasBG_back_url , function(img){
-// 	canvasBack.add(img);
-// 	img.center();
-// });
+//-------------------------------adding objects-------------------------------//
+// canvasFront.add(new fabric.Circle({ radius: 30, fill: '#f55', top: 100, left: 100 }));
+// canvasFront.hoverCursor = 'pointer';
+// this.__canvases.push(canvasFront);
 
+
+function addArt(src){
+	fabric.Image.fromURL(src, function(img) {
+		img.scale(0.2).set({
+			left: 100,
+			top: 100,
+		});
+		canvasFront.add(img).setActiveObject(img);
+	});
+};
+
+// function showArtPreviews(){
+// 	var folder = "assets/img/arts";
+
+// 	$.ajax({
+// 		url : folder,
+// 		success: function (data) {
+// 			$(data).find("a").attr("href", function (i, val) {
+// 				if( val.match(/\.(jpe?g|png|gif)$/) ) { 
+// 					$("body").append( "<img src='"+ folder + val +"'>" );
+// 				} 
+// 			});
+// 		}
+// 	});
+// };
+
+// showArtPreviews();
